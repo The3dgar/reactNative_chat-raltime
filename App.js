7debe43eb879b816491c3ebffd39e55240a81a07
Edgar Olivar <edgarolivar16@gmail.com>
2021-02-08 07:27:45 -0300

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, LogBox, Platform } from "react-native";
+import { Container } from "native-base";
+import * as Font from "expo-font";
+import Login from "./src/screens/Login";
+import Chat from "./src/screens/Chat";
+import { Ionicons } from "@expo/vector-icons";
+
+!(Platform.OS = "web") &&
+  LogBox.ignoreLogs([
+    "Setting a timer for a long period of time",
+    " FIREBASE WARNING: Exception was thrown by user callback",
+  ]);
 
 export default function App() {
+  const [userName, setUserName] = useState(null);
+  const [isLoad, setIsLoad] = useState(false);
+
+  useEffect(() => {
+    Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font,
+    }).then((resp) => {
+      setIsLoad(true);
+    });
+  }, []);
+
+  if (!isLoad)
+    return (
+      <View>
+        <Text>Cargando...</Text>
+      </View>
+    );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container style={styles.container}>
+      {!userName ? (
+        <Login setUserName={setUserName} />
+      ) : (
+        <Chat userName={userName} />
+      )}
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#16202b",
   },
 });
